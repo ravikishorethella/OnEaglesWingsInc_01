a80f5de32c3873715bcaf44e76e2ac182a98dd5b
Ravi Kishore Thella <ravikishorethella@gmail.com>
2018-04-23 03:04:44 -0400

    added board of directors and index page

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
-$(document).ready(function () {
-    $('.mobile a').click(function (e) {
-        $(this).parent().parent().toggleClass('mopen');
-        $(this).html($(this).html() == 'Close X' ? 'Menu' : 'Close X');
-        e.preventDefault();
+$(document).ready(function() {
+  $(".mobile a").click(function(e) {
+    $(this)
+      .parent()
+      .parent()
+      .toggleClass("mopen");
+    $(this).html($(this).html() == "Close X" ? "Menu" : "Close X");
+    e.preventDefault();
+  });
 
+  // code for the user form
+  $.support.placeholder = (function() {
+    var i = document.createElement("input");
+    return "placeholder" in i;
+  })();
+
+  // Hide labels by default if placeholders are supported
+  if ($.support.placeholder) {
+    $(".form-label").each(function() {
+      $(this).addClass("js-hide-label");
     });
 
-    // code for the user form 
-    $.support.placeholder = (function () {
-        var i = document.createElement('input');
-        return 'placeholder' in i;
-    })();
+    // Code for adding/removing classes here
+    $(".form-group")
+      .find("input, textarea")
+      .on("keyup blur focus", function(e) {
+        // Cache our selectors
+        var $this = $(this),
+          $parent = $this.parent().find("label");
 
-    // Hide labels by default if placeholders are supported
-    if ($.support.placeholder) {
-        $('.form-label').each(function () {
-            $(this).addClass('js-hide-label');
-        });
-
-        // Code for adding/removing classes here
-        $('.form-group').find('input, textarea').on('keyup blur focus', function (e) {
-
-            // Cache our selectors
-            var $this = $(this),
-                $parent = $this.parent().find("label");
-
-            if (e.type == 'keyup') {
-                if ($this.val() == '') {
-                    $parent.addClass('js-hide-label');
-                } else {
-                    $parent.removeClass('js-hide-label');
-                }
-            }
-            else if (e.type == 'blur') {
-                if ($this.val() == '') {
-                    $parent.addClass('js-hide-label');
-                }
-                else {
-                    $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
-                }
-            }
-            else if (e.type == 'focus') {
-                if ($this.val() !== '') {
-                    $parent.removeClass('js-unhighlight-label');
-                }
-            }
-        });
-    }
+        if (e.type == "keyup") {
+          if ($this.val() == "") {
+            $parent.addClass("js-hide-label");
+          } else {
+            $parent.removeClass("js-hide-label");
+          }
+        } else if (e.type == "blur") {
+          if ($this.val() == "") {
+            $parent.addClass("js-hide-label");
+          } else {
+            $parent
+              .removeClass("js-hide-label")
+              .addClass("js-unhighlight-label");
+          }
+        } else if (e.type == "focus") {
+          if ($this.val() !== "") {
+            $parent.removeClass("js-unhighlight-label");
+          }
+        }
+      });
+  }
 });
-
